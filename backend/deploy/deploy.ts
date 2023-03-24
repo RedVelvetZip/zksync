ce860d975e45c5d76e950b3f285635dde03aab68
Bradley <bradley.t.sheehan@gmail.com>
2023-03-24 13:24:26 -0600

    deploy NFT

--- backend/deploy/deploy.ts
+++ backend/deploy/deploy.ts
@@ -8,14 +8,14 @@ const PRIV_KEY = fs.readFileSync(".secret").toString()
 
 // An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
-  console.log(`Running deploy script for the Greeter contract`);
+  console.log(`Running deploy script for the NFT contract`);
 
   // Initialize the wallet.
   const wallet = new Wallet(PRIV_KEY);
 
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact("Greeter");
+  const artifact = await deployer.loadArtifact("Red_ERC721");
 
   // Deposit some funds to L2 in order to be able to perform L2 transactions.
 //   const depositAmount = ethers.utils.parseEther("0.001");
@@ -28,31 +28,29 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 //   await depositHandle.wait();
 
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-  // `greeting` is an argument for contract constructor.
-  const greeting = "Hi there!";
-  const greeterContract = await deployer.deploy(artifact, [greeting]);
+  const nftContract = await deployer.deploy(artifact, []);
 
   // Show the contract info.
-  const contractAddress = greeterContract.address;
+  const contractAddress = nftContract.address;
   console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
 
-  // Call the deployed contract.
-  const greetingFromContract = await greeterContract.greet();
-  if (greetingFromContract == greeting) {
-    console.log(`Contract greets us with ${greeting}!`);
-  } else {
-    console.error(`Contract said something unexpected: ${greetingFromContract}`);
-  }
+  // // Call the deployed contract.
+  // const greetingFromContract = await nftContract.greet();
+  // if (greetingFromContract == greeting) {
+  //   console.log(`Contract greets us with ${greeting}!`);
+  // } else {
+  //   console.error(`Contract said something unexpected: ${greetingFromContract}`);
+  // }
 
-  // Edit the greeting of the contract
-  const newGreeting = "Hey guys";
-  const setNewGreetingHandle = await greeterContract.setGreeting(newGreeting);
-  await setNewGreetingHandle.wait();
+  // // Edit the greeting of the contract
+  // const newGreeting = "Hey guys";
+  // const setNewGreetingHandle = await nftContract.setGreeting(newGreeting);
+  // await setNewGreetingHandle.wait();
 
-  const newGreetingFromContract = await greeterContract.greet();
-  if (newGreetingFromContract == newGreeting) {
-    console.log(`Contract greets us with ${newGreeting}!`);
-  } else {
-    console.error(`Contract said something unexpected: ${newGreetingFromContract}`);
-  }
+  // const newGreetingFromContract = await nftContract.greet();
+  // if (newGreetingFromContract == newGreeting) {
+  //   console.log(`Contract greets us with ${newGreeting}!`);
+  // } else {
+  //   console.error(`Contract said something unexpected: ${newGreetingFromContract}`);
+  // }
 }
